edd6186ec8dd02b6af2bcb8f8ad40cd5bac50f29
Doug DeJulio <ddj@cmu.edu>
2014-04-16 09:52:15 -0400

    Initial Commit

---
+++ No D/No_D.h
@@ -0,0 +1,13 @@
+//
+//  No_D.h
+//  No D
+//
+//  Created by Doug DeJulio on 2014-04-16.
+//  Copyright (c) 2014 Doug DeJulio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface No_D : NSObject
+
+@end
---
+++ No D/No_D.m
@@ -0,0 +1,13 @@
+//
+//  No_D.m
+//  No D
+//
+//  Created by Doug DeJulio on 2014-04-16.
+//  Copyright (c) 2014 Doug DeJulio. All rights reserved.
+//
+
+#import "No_D.h"
+
+@implementation No_D
+
+@end
