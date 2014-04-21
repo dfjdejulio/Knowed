6b62c3a86bd909b84ce4b6db17a7fa645eb8259a
Doug DeJulio <ddj@cmu.edu>
2014-04-21 11:30:54 -0400

    Tidying up a bit.

--- Knowed/Knowed.m
+++ Knowed/Knowed.m
@@ -1,12 +1,12 @@
 //
-//  No_D.m
-//  No D
+//  Knowed.m
+//  Knowed
 //
 //  Created by Doug DeJulio on 2014-04-16.
 //  Copyright (c) 2014 Doug DeJulio. All rights reserved.
 //
 
-#import "Knowed.h"
+#import <Knowed/Knowed.h>
 
 @implementation Knowed
 
