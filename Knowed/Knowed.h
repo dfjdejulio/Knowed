a093e35c6916e3bc96e977ee70ce2845ef85e9a4
Doug DeJulio <ddj@cmu.edu>
2014-04-21 11:31:16 -0400

    Split the type definitions out.

--- Knowed/Knowed.h
+++ Knowed/Knowed.h
@@ -1,14 +1,17 @@
 //
-//  No_D.h
-//  No D
+//  Knowed.h
+//  Knowed
 //
 //  Created by Doug DeJulio on 2014-04-16.
 //  Copyright (c) 2014 Doug DeJulio. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <Knowed/KnowedBufferConsole.h>
-#import <Knowed/KnowedConsole.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+#import "Knowed/KnowedTypes.h"
+#import "Knowed/KnowedConsole.h"
+#import "Knowed/KnowedBufferConsole.h"
+#import "Knowed/KnowedUtil.h"
 
 @interface Knowed : NSObject
 
---
+++ Knowed/KnowedTypes.h
@@ -0,0 +1,19 @@
+//
+//  KnowedTypes.h
+//  Knowed
+//
+//  Created by Doug DeJulio on 2014-04-21.
+//  Copyright (c) 2014 Doug DeJulio. All rights reserved.
+//
+
+#ifndef Knowed_KnowedTypes_h
+#define Knowed_KnowedTypes_h
+
+#import <Foundation/Foundation.h>
+
+typedef void (^KnowedOutputBlock)(NSString *message);
+
+typedef NSString * (^KnowedInputBlockWithMessage)(NSString *message);
+
+
+#endif
