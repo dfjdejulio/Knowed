bcc36ecd7687a1806c38c78f116ab6f5a6b02778
Doug DeJulio <ddj@cmu.edu>
2014-05-21 16:21:49 -0400

    Basically working.

---
+++ KnowedLibTests/KnowedLibTests.m
@@ -0,0 +1,34 @@
+//
+//  KnowedLibTests.m
+//  KnowedLibTests
+//
+//  Created by Doug DeJulio on 2014-05-21.
+//  Copyright (c) 2014 Doug DeJulio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KnowedLibTests : XCTestCase
+
+@end
+
+@implementation KnowedLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
