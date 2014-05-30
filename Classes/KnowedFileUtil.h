c48414df80e0aeda2ff7b1d1301af1b0822fd56c
Doug DeJulio <ddj@aisb.org>
2014-05-30 18:30:10 -0400

    headerdoc/appledoc

--- Classes/KnowedFileUtil.h
+++ Classes/KnowedFileUtil.h
@@ -8,12 +8,32 @@
 
 #import <Knowed/KnowedThingy.h>
 
+/*!
+ This protocol defines the KnowedFileUtil members that should be exposed
+ to JavaScript.
+ */
+
 @protocol KnowedFileUtilExports <JSExport>
+/*!
+ Import the contents of a URL into a string.
+ @param urlstring The URL to fetch things from.
+ @return The string with the contents from that URL.
+ */
 - (NSString *)importUrl: (NSString *)urlstring;
+/*!
+ Import the contents of a file into a string.
+ @param filename The name of the file.
+ */
 - (NSString *)import: (NSString *)filename;
+/*!
+ Load and execute the contents of a file, like Rhino's
+ builtin "load()" function.
+ @param filename The file to load.
+ */
 - (JSValue *) load: (NSString *) filename;
 @end
 
+/*! Provide basic file I/O operations for JavaScriptCore. */
 @interface KnowedFileUtil : KnowedThingy <KnowedFileUtilExports>
 
 @end
